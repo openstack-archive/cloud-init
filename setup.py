630018e02dc3eb0a74280d56c4245b0ccdfaefd7
Joshua Harlow <harlowja@yahoo-inc.com>
2015-04-01 10:46:24 -0700

    Fix all the current brokeness
    Add missing files and tweaks so that the automated
    CI/flake8/pep8/testr/docs and such work correctly.
    
    Change-Id: I838b02bf0037569065b7c7914f4a68b92d7ebd6a

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+
 import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
 setuptools.setup(
     setup_requires=['pbr'],
