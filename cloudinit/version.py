024d0ffbe288d01109c8b441d8f4fcaf9b050da3
Joshua Harlow <harlowja@yahoo-inc.com>
2015-04-02 12:04:50 -0700

    Bring over the 'url_helper' module from bzr
    This module is quite useful for fetching urls in a manner
    that can be provided custom headers, retries and ssl_args
    so let's ensure we have it available for usage under these
    circumstances.
    
    Also adds some basic sanity tests so that it will continue
    to work as expected.
    
    Change-Id: I4d5e777cf530e822bbea69c76846ebebb48a7927

---
+++ cloudinit/version.py
@@ -0,0 +1,14 @@
+# Copyright 2015 Canonical Ltd.
+# This file is part of cloud-init.  See LICENCE file for license information.
+#
+# vi: ts=4 expandtab
+
+import pkg_resources
+
+try:
+    from pbr import version as pbr_version
+    _version_info = pbr_version.VersionInfo('cloudinit')
+    version_string = _version_info.version_string
+except ImportError:
+    _version_info = pkg_resources.get_distribution('cloudinit')
+    version_string = lambda: _version_info.version
