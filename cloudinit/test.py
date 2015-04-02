183ce82c8142c21b3c026e87b08611fb51755c25
Joshua Harlow <harlowja@yahoo-inc.com>
2015-04-02 12:04:50 -0700

    Bring over the 'url_helper' module from bzr
    This module is quite useful for fetching urls in a manner
    that can be provided custom headers; retries and ssl args
    so let's ensure we have it available for usage under these
    circumstances.
    
    Also adds some basic sanity tests so that it will continue
    to work as expected.
    
    Change-Id: I4d5e777cf530e822bbea69c76846ebebb48a7927

---
+++ cloudinit/test.py
@@ -0,0 +1,24 @@
+#    Copyright (C) 2015 Yahoo!
+#
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# vi: ts=4 expandtab
+
+
+import testtools
+
+
+class TestCase(testtools.TestCase):
+    """Base class for all cloud-init test cases."""
