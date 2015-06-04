dd776afe67e906c6205901038b702f9c10718ed5
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-06-04 12:42:16 +0300

    Add the data source base classes and the HTTP OpenStack implementation
    Change-Id: Ia5fdbe6cbd6364f8ec671fec413fdd37741a2598

--- cloudinit/osys/windows/network.py
+++ cloudinit/osys/windows/network.py
@@ -11,11 +11,15 @@ from ctypes import wintypes
 import logging
 import subprocess
 
+from six.moves import urllib_parse
+
 from cloudinit import exceptions
+from cloudinit.osys import base
 from cloudinit.osys import network
 from cloudinit.osys.windows.util import iphlpapi
 from cloudinit.osys.windows.util import kernel32
 from cloudinit.osys.windows.util import ws2_32
+from cloudinit import url_helper
 
 
 MIB_IPPROTO_NETMGMT = 3
@@ -26,7 +30,8 @@ _PROTOCOL_TCP = "TCP"
 _PROTOCOL_UDP = "UDP"
 _ERROR_FILE_NOT_FOUND = 2
 _ComputerNamePhysicalDnsHostname = 5
-LOG = logging.getLogger(__file__)
+_MAX_URL_CHECK_RETRIES = 3
+LOG = logging.getLogger(__name__)
 
 
 def _heap_alloc(heap, size):
@@ -35,6 +40,15 @@ def _heap_alloc(heap, size):
         raise exceptions.CloudInitError(
             'Unable to allocate memory for the IP forward table')
     return table_mem
+
+
+def _check_url(url, retries_count=_MAX_URL_CHECK_RETRIES):
+    LOG.debug("Testing url: %s", url)
+    try:
+        url_helper.read_url(url, retries=retries_count)
+        return True
+    except url_helper.UrlError:
+        return False
 
 
 class Network(network.Network):
@@ -109,6 +123,42 @@ class Network(network.Network):
         """
         return next((r for r in self.routes() if r.destination == '0.0.0.0'),
                     None)
+
+    def set_metadata_ip_route(self, metadata_url):
+        """Set a network route if the given metadata url can't be accessed.
+
+        This is a workaround for
+           https://bugs.launchpad.net/quantum/+bug/1174657.
+        """
+        osutils = base.get_osutils()
+
+        if osutils.general.check_os_version(6, 0):
+            # 169.254.x.x addresses are not getting routed starting from
+            # Windows Vista / 2008
+            metadata_netloc = urllib_parse.urlparse(metadata_url).netloc
+            metadata_host = metadata_netloc.split(':')[0]
+
+            if not metadata_host.startswith("169.254."):
+                return
+
+            routes = self.routes()
+            exists_route = any(route.destination == metadata_host
+                               for route in routes)
+            if not exists_route and not _check_url(metadata_url):
+                default_gateway = self.default_gateway()
+                if default_gateway:
+                    try:
+                        LOG.debug('Setting gateway for host: %s',
+                                  metadata_host)
+                        route = Route(
+                            destination=metadata_host,
+                            netmask="255.255.255.255",
+                            gateway=default_gateway.gateway,
+                            interface=None, metric=None)
+                        Route.add(route)
+                    except Exception as ex:
+                        # Ignore it
+                        LOG.exception(ex)
 
     # These are not required by the Windows version for now,
     # but we provide them as noop version.
