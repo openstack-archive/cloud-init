88479236c2b6664c94ca4cde44a938fab878474a
Scott Moser <smoser@ubuntu.com>
2015-07-16 14:49:31 -0400

    add cloud-init main
    This is start of the cloud-init main executable.
    
    Change-Id: I795e1d279a99476fb6cd33d1e7ad6a7af0978c8d

--- cloudinit/shell.py
+++ cloudinit/shell.py
@@ -3,10 +3,55 @@
 #
 # vi: ts=4 expandtab
 
+import argparse
+import sys
 
-def main():
-    print("Run cloudinit run!")
+
+def populate_parser(parser, common, subcommands, func_namespace=None):
+    if func_namespace is None:
+        func_namespace = globals()
+
+    for (args, kwargs) in common:
+        parser.add_argument(*args, **kwargs)
+
+    subparsers = parser.add_subparsers()
+    for subcmd in sorted(subcommands.keys()):
+        val = subcommands[subcmd]
+        func = val.get('func')
+        if not func:
+            func = func_namespace['main_' + subcmd.replace("-", "_")]
+        sparser = subparsers.add_parser(subcmd, help=val['help'])
+        sparser.set_defaults(func=func)
+        for (args, kwargs) in val['opts']:
+            sparser.add_argument(*args, **kwargs)
+
+
+def main(args=sys.argv):
+    parser = argparse.ArgumentParser(prog='cloud-init')
+
+    populate_parser(parser, COMMON_ARGS, SUBCOMMANDS)
+    parsed = parser.parse_args(args[1:])
+
+    parsed.func(parsed)
+    return 0
+
+
+def main_version(args):
+    sys.stdout.write("cloud-init 1.9\n")
+    return
+
+
+COMMON_ARGS = [
+    (('--verbose', '-v'), {'action': 'count', 'default': 0}),
+]
+
+SUBCOMMANDS = {
+    'version': {
+        'opts': [],
+        'help': 'print cloud-init version',
+    },
+}
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
---
+++ cloudinit/tests/test_shell.py
@@ -0,0 +1,36 @@
+# Copyright 2015 Canonical Ltd.
+# This file is part of cloud-init.  See LICENCE file for license information.
+#
+# vi: ts=4 expandtab
+
+import cloudinit.shell as shell
+
+from cloudinit.test import TestCase
+from cloudinit.tests.util import mock
+
+
+class TestCase(TestCase):
+
+    @mock.patch('cloudinit.shell.main_version')
+    def test_main_has_version(self, main_version):
+        shell.main(args=['cloud-init', 'version'])
+        self.assertEqual(main_version.call_count, 1)
+
+    def test_help_exits_success(self):
+        with mock.patch('cloudinit.shell.sys.stdout'):
+            exc = self.assertRaises(
+                SystemExit, shell.main, args=['cloud-init', '--help'])
+            self.assertEqual(exc.code, 0)
+
+    def test_invalid_arguments_exit_fail(self):
+        # silence writes that get to stderr
+        with mock.patch('cloudinit.shell.sys.stderr'):
+            exc = self.assertRaises(
+                SystemExit, shell.main, args=['cloud-init', 'bogus_argument'])
+            self.assertNotEqual(exc.code, 0)
+
+    @mock.patch('cloudinit.shell.sys.stdout')
+    def test_version_shows_cloud_init(self, mock_out_write):
+        shell.main(args=['cloud-init', 'version'])
+        write_arg = mock_out_write.write.call_args[0][0]
+        self.assertTrue(write_arg.startswith('cloud-init'))
