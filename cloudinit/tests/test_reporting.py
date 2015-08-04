0b770823b1f3dc859917f5d4e0a0990c5ba6d954
Scott Moser <smoser@ubuntu.com>
2015-08-04 07:29:22 -0500

    add ReportingEventStack
    This adds a new class ReportingEvent Stack for using report_start_event and
    report_finish_event easily with a context handler.
    
    It also modifies FinishReportingEvent (and finish_event) accordingly to take a
    status rather than simply a boolean successful.  The intent is that WARN
    is provided when a non-desireable result occurred but it is non-fatal.
    
    Change-Id: I978c76e429790036f8740d7eb7279e925a1e74d0

--- cloudinit/tests/test_reporting.py
+++ cloudinit/tests/test_reporting.py
@@ -4,7 +4,6 @@
 # vi: ts=4 expandtab
 
 from cloudinit import reporting
-from cloudinit.reporting import handlers
 from cloudinit.tests import TestCase
 from cloudinit.tests.util import mock
 
@@ -33,10 +32,10 @@ class TestReportStartEvent(TestCase):
 
 class TestReportFinishEvent(TestCase):
 
-    def _report_finish_event(self, successful=None):
+    def _report_finish_event(self, result=reporting.status.SUCCESS):
         event_name, event_description = 'my_test_event', 'my description'
         reporting.report_finish_event(
-            event_name, event_description, successful=successful)
+            event_name, event_description, result=result)
         return event_name, event_description
 
     def assertHandlersPassedObjectWithAsString(
@@ -52,7 +51,8 @@ class TestReportFinishEvent(TestCase):
             self, instantiated_handler_registry):
         event_name, event_description = self._report_finish_event()
         expected_string_representation = ': '.join(
-            ['finish', event_name, event_description])
+            ['finish', event_name, reporting.status.SUCCESS,
+             event_description])
         self.assertHandlersPassedObjectWithAsString(
             instantiated_handler_registry.registered_items,
             expected_string_representation)
@@ -62,9 +62,10 @@ class TestReportFinishEvent(TestCase):
     def test_reporting_successful_finish_has_sensible_string_repr(
             self, instantiated_handler_registry):
         event_name, event_description = self._report_finish_event(
-            successful=True)
+            result=reporting.status.SUCCESS)
         expected_string_representation = ': '.join(
-            ['finish', event_name, 'success', event_description])
+            ['finish', event_name, reporting.status.SUCCESS,
+             event_description])
         self.assertHandlersPassedObjectWithAsString(
             instantiated_handler_registry.registered_items,
             expected_string_representation)
@@ -74,9 +75,9 @@ class TestReportFinishEvent(TestCase):
     def test_reporting_unsuccessful_finish_has_sensible_string_repr(
             self, instantiated_handler_registry):
         event_name, event_description = self._report_finish_event(
-            successful=False)
+            result=reporting.status.FAIL)
         expected_string_representation = ': '.join(
-            ['finish', event_name, 'fail', event_description])
+            ['finish', event_name, reporting.status.FAIL, event_description])
         self.assertHandlersPassedObjectWithAsString(
             instantiated_handler_registry.registered_items,
             expected_string_representation)
@@ -96,31 +97,32 @@ class TestReportingHandler(TestCase):
 
     def test_no_default_publish_event_implementation(self):
         self.assertRaises(NotImplementedError,
-                          handlers.ReportingHandler().publish_event, None)
+                          reporting.handlers.ReportingHandler().publish_event,
+                          None)
 
 
 class TestLogHandler(TestCase):
 
-    @mock.patch.object(handlers.logging, 'getLogger')
+    @mock.patch.object(reporting.handlers.logging, 'getLogger')
     def test_appropriate_logger_used(self, getLogger):
         event_type, event_name = 'test_type', 'test_name'
         event = reporting.ReportingEvent(event_type, event_name, 'description')
-        handlers.LogHandler().publish_event(event)
+        reporting.handlers.LogHandler().publish_event(event)
         self.assertEqual(
             [mock.call(
                 'cloudinit.reporting.{0}.{1}'.format(event_type, event_name))],
             getLogger.call_args_list)
 
-    @mock.patch.object(handlers.logging, 'getLogger')
+    @mock.patch.object(reporting.handlers.logging, 'getLogger')
     def test_single_log_message_at_info_published(self, getLogger):
         event = reporting.ReportingEvent('type', 'name', 'description')
-        handlers.LogHandler().publish_event(event)
+        reporting.handlers.LogHandler().publish_event(event)
         self.assertEqual(1, getLogger.return_value.info.call_count)
 
-    @mock.patch.object(handlers.logging, 'getLogger')
+    @mock.patch.object(reporting.handlers.logging, 'getLogger')
     def test_log_message_uses_event_as_string(self, getLogger):
         event = reporting.ReportingEvent('type', 'name', 'description')
-        handlers.LogHandler().publish_event(event)
+        reporting.handlers.LogHandler().publish_event(event)
         self.assertIn(event.as_string(),
                       getLogger.return_value.info.call_args[0][0])
 
@@ -131,7 +133,7 @@ class TestDefaultRegisteredHandler(TestCase):
         registered_items = (
             reporting.instantiated_handler_registry.registered_items)
         for _, item in registered_items.items():
-            if isinstance(item, handlers.LogHandler):
+            if isinstance(item, reporting.handlers.LogHandler):
                 break
         else:
             self.fail('No reporting LogHandler registered by default.')
@@ -191,3 +193,76 @@ class TestReportingConfiguration(TestCase):
         expected_handler_config = handler_config.copy()
         reporting.add_configuration({'my_test_handler': handler_config})
         self.assertEqual(expected_handler_config, handler_config)
+
+
+class TestReportingEventStack(TestCase):
+    @mock.patch('cloudinit.reporting.report_finish_event')
+    @mock.patch('cloudinit.reporting.report_start_event')
+    def test_start_and_finish_success(self, report_start, report_finish):
+        with reporting.ReportEventStack(name="myname", description="mydesc"):
+            pass
+        self.assertEqual(report_start.call_count, 1)
+        report_start.assert_called_with("myname", "mydesc")
+        self.assertEqual(report_finish.call_count, 1)
+        report_finish.assert_called_with(
+            "myname", "mydesc", reporting.status.SUCCESS)
+
+    @mock.patch('cloudinit.reporting.report_finish_event')
+    @mock.patch('cloudinit.reporting.report_start_event')
+    def test_finish_exception_defaults_fail(self, report_start, report_finish):
+        name = "myname"
+        desc = "mydesc"
+        try:
+            with reporting.ReportEventStack(name, description=desc):
+                raise ValueError("This didnt work")
+        except ValueError:
+            pass
+        self.assertEqual(report_start.call_count, 1)
+        report_start.assert_called_with(name, desc)
+        self.assertEqual(report_finish.call_count, 1)
+        report_finish.assert_called_with(name, desc, reporting.status.FAIL)
+
+    @mock.patch('cloudinit.reporting.report_finish_event')
+    @mock.patch('cloudinit.reporting.report_start_event')
+    def test_stop_exception_explicit_set(self, report_start, report_finish):
+        name = "myname"
+        desc = "mydesc"
+        try:
+            with reporting.ReportEventStack(
+                    name, desc, result_on_exception=reporting.status.WARN):
+                raise ValueError("This didnt work")
+        except ValueError:
+            pass
+        self.assertEqual(report_start.call_count, 1)
+        report_start.assert_called_with(name, desc)
+        self.assertEqual(report_finish.call_count, 1)
+        report_finish.assert_called_with(name, desc, reporting.status.WARN)
+
+    @mock.patch('cloudinit.reporting.report_start_event')
+    def test_child_fullname_respects_parent(self, report_start):
+        parent_name = "topname"
+        c1_name = "c1name"
+        c2_name = "c2name"
+        c2_expected_fullname = '/'.join([parent_name, c1_name, c2_name])
+        c1_expected_fullname = '/'.join([parent_name, c1_name])
+
+        parent = reporting.ReportEventStack(parent_name, "topdesc")
+        c1 = reporting.ReportEventStack(c1_name, "c1desc", parent=parent)
+        c2 = reporting.ReportEventStack(c2_name, "c2desc", parent=c1)
+
+        with c1:
+            report_start.assert_called_with(c1_expected_fullname, "c1desc")
+            with c2:
+                report_start.assert_called_with(c2_expected_fullname, "c2desc")
+
+    @mock.patch('cloudinit.reporting.report_finish_event')
+    @mock.patch('cloudinit.reporting.report_start_event')
+    def test_child_result_bubbles_up(self, report_start, report_finish):
+        parent = reporting.ReportEventStack("topname", "topdesc")
+        child = reporting.ReportEventStack("c_name", "c_desc", parent=parent)
+        with parent:
+            with child:
+                child.result = reporting.status.WARN
+
+        report_finish.assert_called_with(
+            "topname", "topdesc", reporting.status.WARN)
